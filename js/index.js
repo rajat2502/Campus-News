46909ac7415279ea5de6747c28fb58d37a2cdc85
abc <rajat2502@users.noreply.github.com>
2019-07-03 18:09:05 +0530

    ncs page improved

--- js/index.js
+++ js/index.js
@@ -4,12 +4,22 @@ document.getElementById('sidebar').addEventListener('mouseover', function(){
   document.querySelectorAll('.name').forEach(function(element){
     element.style.display = "inline";
   })
+
   document.getElementsByClassName('overlay')[0].style.display = "block";
+
+  document.querySelectorAll('#sidebar ul li').forEach(function(element){
+    element.style.padding = "4px 12px 4px 12px";
+  })
 });
 
 document.getElementById('sidebar').addEventListener('mouseout', function(){
   document.querySelectorAll('.name').forEach(function(element){
     element.style.display = "none";
   })
+
   document.getElementsByClassName('overlay')[0].style.display = "none";
+
+  document.querySelectorAll('#sidebar ul li').forEach(function(element){
+    element.style.padding = "4px 12px 4px 13px";
+  })
 });
